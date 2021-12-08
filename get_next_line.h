2960fa878254b45de905f08100e4e99a7317efb7
Ilona Shakurova <leverseau19@gmail.com>
2021-12-08 09:18:12 +0200

    Add changes to fd count

--- get_next_line.h
+++ get_next_line.h
@@ -6,20 +6,20 @@
 /*   By: ishakuro <ishakuro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/26 12:38:39 by ishakuro          #+#    #+#             */
-/*   Updated: 2021/12/06 22:25:41 by ishakuro         ###   ########.fr       */
+/*   Updated: 2021/12/08 09:18:01 by ishakuro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# ifndef GET_NEXT_LINE_H
-#define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-#include <stdlib.h>
-#include <unistd.h>
-#include "libft.h"
+# include <stdlib.h>
+# include <unistd.h>
+# include "libft.h"
 
-#define BUFF_SIZE 4096
-#define FD_COUNT 4096
+# define BUFF_SIZE 32
+# define FD_COUNT 12228
 
-int get_next_line(const int fd, char **line);
+int	get_next_line(const int fd, char **line);
 
 #endif
